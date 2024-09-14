a45a9a0becb01d5927dca82408d2d727d831fdd6
JMV2071 <juanmv2071@gmail.com>
2024-09-13 23:00:05 -0300

    TP1 version 2

--- TP1-main.py
+++ TP1-main.py
@@ -21,3 +21,38 @@ print("Matricula: " + str(matricula))
 print("Cuota: " + str(cuota))
 print("Arancel mensual materia 'Python I': $" + str(PythonI_xmes))
 print("Arancel mensual materia 'Python I' con descuento: $" + str(PythonI_xmes_descuento))
+
+#ejercicio 2
+print("=====================================")
+print("==========notas de examenes==========")
+print("=====================================")
+
+par1 = float(input('Ingrese la nota del primer parcial: '))
+par2 = float(input('Ingrese la nota del segundo parcial: '))
+
+prom = str((par1 + par2)/2)
+print("el promedio de las notas es: " + prom)
+
+if par2 >= 7:
+    print("Aprobó el segundo parcial")
+else:
+    print("Desaprobó el segundo parcial")
+
+if par1 == par2:
+    print("Progreso del 1er al 2do parcial: mantuvo su desempeño")
+elif par1 > par2:
+    print("Progreso del 1er al segundo parcial: empeoró su desempeño")
+elif par1< par2:
+    print("Progreso del 1er al 2do parcial: mejoró su desempeño")
+else:
+    print ("error")
+
+prom = float(prom)
+
+if prom >= 7:
+    print("Promocionó la materia")
+else:
+    if prom >=4:
+        print("Debe rendir exámen final")
+    else:
+        print("Debe recursar la materia")
